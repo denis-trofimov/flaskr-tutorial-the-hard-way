4c6b9bb5d9ff685adebf90dbb68bd614ff83ca82
Denis Trofimov <silaradost@yandex.ru>
2019-01-06 19:36:42 +0300

    Login and logout views.

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -28,8 +28,7 @@ def register():
         elif not password:
             error = "Password is requered."
         elif db.execute(
-            "SELECT id FROM user WHERE username = ?",
-            (username, )
+            "SELECT id FROM user WHERE username = ?", (username, )
         ).fetchone() is not None:
             error = "User {} is already registered".format(username)
         else:
@@ -43,3 +42,47 @@ def register():
         flash(error)
     return render_template("auth/register.html")
 
+
+@bp.route("/loginogin", methods=("GET", "POST"))
+def login():
+    """Login user view."""
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        db = get_db()
+        error = None
+        user = db.execute(
+            "SELECT id FROM user WHERE username = ?", (username, )
+        ).fetchone()
+
+        if user is None:
+            error = "Incorrect username."
+        elif not check_password_hash(user["password"], password):
+            error = "Incorrect password"
+
+        if error is None:
+            session.clear()
+            session["user_id"] = user["id"]
+            return redirect(url_for("index"))
+
+        flash(error)
+
+    return render_template("auth/login.html")
+
+@bp.before_app_request
+def load_logged_in_user():
+    """Get session's user info from DB for the length of the request."""
+    user_id = session.get("user_id")
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = get_db.execute(
+            "SELECT * FROM user WHERE id = ?", (user_id,)
+        ).fetchone()
+
+@bp.route("/logout")
+def logout():
+    """Remove the user id from the session."""
+    session.clear()
+    return redirest(url_for("index"))
